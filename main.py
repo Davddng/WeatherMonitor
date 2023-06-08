88ce91a2b62bf0fab66d2856ce6fd31eb5cfce74
David Ding <davddng@gmail.com>
2023-06-08 16:34:02 -0600

    initial air quality implementation test

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import serial
 
+from flask import Flask, request, jsonify
 from PMS7003 import readData
 from time import sleep
 
 def main():
-    ser = serial.Serial("/dev/ttyS0", 9600)
+    PMS7003_SER = serial.Serial("/dev/ttyS0", 9600)
 
-    counter = 0
-    while True:
-        data = readData(ser)
-        if counter == 5:
-            break
+    # Web APIs
+    app = Flask(__name__)
 
-        counter += 1
-        print("Reading {0}".format(counter))
-        print("PM1.0: {0}".format(data['pm1']))
-        print("PM2.5: {0}".format(data['pm25']))
-        print("PM10: {0}".format(data['pm10']))
+    @app.get("/air_quality")
+    def getAirQuality():
+        airData = readData(PMS7003_SER)
+        return jsonify(airData)
 
-        sleep(1)
+    @app.post("/countries")
+    def add_country():
+        if request.is_json:
+            country = request.get_json()
+            country["id"] = _find_next_id()
+            countries.append(country)
+            return country, 201
+        return {"error": "Request must be JSON"}, 415
+    
+    app.run()
 
 if __name__ == '__main__':
     main()
