3fa70be6f5b82ff36d9b7182aa1c8347eb25f8e9
Davddng <davddng@gmail.com>
2023-08-16 01:08:57 -0600

    moved to picam2

--- IMX219.py
+++ IMX219.py
@@ -1,4 +1,4 @@
-from picamera import PiCamera
+from picamera2 import Picamera2
 import time
 import os
 
@@ -6,10 +6,15 @@ def takePhoto(path):
     script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
     abs_file_path = os.path.join(script_dir, path)
 
-    camera = PiCamera()
+    camera = Picamera2()
+    picture_config = camera.create_still_configuration()
+    camera.configure(picture_config)
+    
+    camera.start()
     time.sleep(5)
-    camera.capture(abs_file_path)
+    print(abs_file_path)
+    camera.capture_file(abs_file_path)
     # print("Done.")
-
+    camera.close()
 if __name__ == '__main__':
     takePhoto("test.jpg")
