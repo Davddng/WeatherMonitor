b1baed2afa36e296b38d6e69704012dd193fa213
David Ding <davddng@gmail.com>
2023-08-16 00:42:53 -0600

    camer bug fix

--- IMX219.py
+++ IMX219.py
@@ -1,10 +1,14 @@
 from picamera import PiCamera
 import time
+import os
 
 def takePhoto(path):
+    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    abs_file_path = os.path.join(script_dir, path)
+
     camera = PiCamera()
     time.sleep(5)
-    camera.capture(path)
+    camera.capture(abs_file_path)
     # print("Done.")
 
 if __name__ == '__main__':
