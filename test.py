90f59543c2e0cc0f247046619fb3612a5df80f2d
Davddng <davddng@gmail.com>
2023-11-06 20:40:26 -0700

    cors fix

--- test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import time
-
-async def main(name="default", **kwargs):
-    print(name)
-    print(kwargs)
-
-if __name__ == "__main__":
-    asyncio.run(main(name="testName", var1="testVar1", var2="testVar2"))
