ccc127894721289266ffc73951a467bbe892d4f0
Davddng <davddng@gmail.com>
2024-01-10 23:35:26 -0700

    bug fix

--- BluetoothReader.py
+++ BluetoothReader.py
@@ -152,7 +152,8 @@ class BLEReader:
     # Helper functions to print Bluetooth attributes for debugging
     async def printDescriptorDetails(self, descriptor):
         self.debugLog(f"Descriptor: {descriptor.description}")
-        self.debugLog(f"Descriptor value: {await self._BLE_CLIENT.read_gatt_descriptor(descriptor.handle)}")
+        if self._BLE_CLIENT.is_connected:
+            self.debugLog(f"Descriptor value: {await self._BLE_CLIENT.read_gatt_descriptor(descriptor.handle)}")
 
     async def printCharacteristicDetails(self, characteristic):
         self.debugLog(f"Characteristic description: {characteristic.description}")
