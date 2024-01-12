9f3546650bb4517d19bfba2f672f1413b52d6c95
Davddng <davddng@gmail.com>
2024-01-11 23:50:33 -0700

    reconnect manually

--- BluetoothReader.py
+++ BluetoothReader.py
@@ -117,7 +117,7 @@ class BLEReader:
     def clientDisconnectHandler(self, client):
         self.ready = False
         self.debugLog(f"Client disconnected: {client}")
-        self._BLE_CLIENT = BleakClient(address_or_ble_device = self.foundDevices[0], disconnected_callback = self.clientDisconnectHandler)
+        # self._BLE_CLIENT = BleakClient(address_or_ble_device = self.foundDevices[0], disconnected_callback = self.clientDisconnectHandler)
         # await self.connectToDevice()
         # asyncio.run(self.subscribeCharacteristics(debug=False))
 
@@ -152,7 +152,7 @@ class BLEReader:
         for name, characteristic in self.characteristics.items():
             try:
                 async with asyncio.timeout(1):
-                    await self._BLE_CLIENT.stop_notify(characteristic.uuid)
+                    # await self._BLE_CLIENT.stop_notify(characteristic.uuid)
                     await self._BLE_CLIENT.start_notify(characteristic.uuid, self.characteristicUpdate)
             except Exception as e:
                 self.debugLog("Subscribe error")
